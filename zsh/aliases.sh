e4f3263db39477499f81a82c36c2f9f497b12d95
Benjamin Jacobs <benjammin1100@gmail.com>
2020-01-06 12:07:37 -0700

    make tree use less

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -7,9 +7,31 @@ alias disk="df -h | grep -E 'File|sd'"
 
 alias dotupdate="$DOTFILES/update.sh"
 
+alias tree="lesstree"
+
 alias urmumbiggay="echo no u"
 
 if [[ $CONFIG_DOT_NEOVIM == "YES" ]]; then
   alias vim='nvim'
   alias vi='nvim'
 fi
+
+function lesstree() {
+  # Check if the function is being piped
+  if [ -t 1 ]; then
+    # Terminal
+
+    # Check if the tree command is larger than the terminal window
+    TREE=$("tree" -C $@)
+    if [ $(echo $TREE | wc -l) -gt $(tput lines) ]; then
+      # If it is larger, use less to allow scrolling
+      echo $TREE | less -R
+    else
+      # Why use less, when the output is smaller than the terminal
+      echo $TREE
+    fi
+  else
+    # Pipe, output normal tree
+    "tree" $@
+  fi
+}
