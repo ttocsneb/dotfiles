f279e58d5b6ee75cc53c19341da2115e012ea301
Benjamin Jacobs <benjammin1100@gmail.com>
2020-01-08 15:37:10 -0700

    fix dotrc setup

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -18,24 +18,26 @@ echo ====================
 
 read -p "Will your terminal support 256 colors? (Y/n)" colors
 CONFIG_TM_THEME=ttocsnebtheme
+TERM_TYPE=xterm
 if ! is_no "$colors"; then
-  echo 'export TERM="xterm-256color"' >> $config
+  TERM_TYPE=xterm-256color
   CONFIG_TM_THEME=nerdtheme
 fi
-echo "CONFIG_DOT_THEME=$CONFIG_TM_THEME" >> $config
+echo "export TERM=\"$TERM_TYPE\"" >> $config
+echo "export CONFIG_DOT_THEME=$CONFIG_TM_THEME" >> $config
 
 read -p "Should tmux display the battery? (y/N)" battery
 CONFIG_BATTERY=YES
 if ! is_yes "$battery"; then
   CONFIG_BATTERY=NO
 fi
-echo "CONFIG_DOT_BATTERY=$CONFIG_BATTERY" >> $config
+echo "export CONFIG_DOT_BATTERY=$CONFIG_BATTERY" >> $config
 
 read -p "Should tmux display the current song? (y/N)" song
 CONFIG_MUSIC=YES
 if ! is_yes "$song"; then
   CONFIG_MUSIC=NO
 fi
-echo "CONFIG_DOT_MUSIC=$CONFIG_MUSIC" >> $config
+echo "export CONFIG_DOT_MUSIC=$CONFIG_MUSIC" >> $config
 
 echo --------------------
